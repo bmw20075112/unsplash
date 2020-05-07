7dcf291cf51aa7f2bf4f23a00220e695f26eaec6
Barney Zhang <bmw20075112@gmail.com>
2020-05-07 18:04:59 +0800

    0507 firebase connect

---
+++ src/fetch/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyD-2utHSuH0KKUTthIbkU1WEDe5G0Dh4L4",
+    authDomain: "unsplash-28878.firebaseapp.com",
+    databaseURL: "https://unsplash-28878.firebaseio.com",
+    projectId: "unsplash-28878",
+    storageBucket: "unsplash-28878.appspot.com",
+    messagingSenderId: "95972940438",
+    appId: "1:95972940438:web:9de60bdf7c4572f5d440b7"
+};
+// Initialize Firebase
+const firebaseApp=firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore();
--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -2,11 +2,17 @@
     <section>
         <ul class="pics-align">
             <li class="pic-wrapper" v-for="pic in pics" :key="pic.id">
-                <img :src="pic.urls.small" :alt="pic.alt_description">
+                <img class="pic" :src="pic.urls.small" :alt="pic.alt_description">
                 <div class="hover-info">
-                    <img class="user-image" :src="pic.user.profile_image.small"
-                    alt="Author Name">
-                    <p class="user-name">{{pic.user.name}}</p>
+                    <div class="author">
+                        <img class="user-image" :src="pic.user.profile_image.small"
+                        alt="Author Name">
+                        <p class="user-name">{{pic.user.name}}</p>
+                    </div>
+
+                    <div class="options">
+                        <i class="fas fa-heart" :class="{'fas':true}"></i>
+                    </div>
                 </div>
             </li>
         </ul>
@@ -14,10 +20,11 @@
 </template>
 
 <script>
+import db from '../fetch/firebase';
 export default {
     data() {
         return {
-
+            likeList:{}
         }
     },
 
@@ -28,8 +35,23 @@ export default {
 
         url(){
             return this.$store.getters.pics.urls;
+        },
+
+        like(){
+            if(true){
+                return{
+                'fas':true
+                }
+            }
+            
         }
-    }
+    },
+
+    // created(){
+    //     db.collection('user').doc('hi').onSnapshot(snapshot=>{
+    //         snapshot.
+    //     })
+    // }
 }
 </script>
 
@@ -41,12 +63,14 @@ export default {
 }
 
 .user-image{
-    border-radius: 50%;
+    border-radius: 100%;
     display: inline-block;
 }
 
 .user-name{
     display: inline-block;
+    margin-left: 0.5rem;
+    color: white;
 }
 
 .pic-wrapper{
@@ -56,16 +80,39 @@ export default {
     &:hover .hover-info{
         visibility: visible;
     }
+    &:hover .pic{
+        filter: brightness(80%);
+    }
+}
+
+.pic{
+    transition:all 0.5s ease;
 }
 
 .hover-info{
-    position: absolute;
     visibility: hidden;
+    position: absolute;
+    width: 100%;
+    top: 1rem;
     display: flex;
-    align-items: center;
-    bottom: 5rem;
+    justify-content: space-between;
+    align-items: flex-end;
 }
 
+.author{
+    display: flex;
+    align-items: center;
+    margin-left: 1rem;
+}
+
+.options{
+    margin-right: 1rem;;
+}
+
+.fa-heart{
+    color: white;
+    font-size: 1.6rem;
+}
 // @media only screen and (min-width:$large){
 //     .pics-align{
 //         column-count: 4;
